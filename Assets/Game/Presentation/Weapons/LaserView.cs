abf7ea899c14830b80936613270de991716e2940
Ilya Kharlamov <ilya.kharlamov.92@mail.ru>
2026-05-31 23:22:48 +0300

    replaced UniTaskVoid with UniTask

--- Assets/Game/Presentation/Weapons/LaserView.cs
+++ Assets/Game/Presentation/Weapons/LaserView.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using Game.Core.Signals;
 using UnityEngine;
@@ -11,6 +12,7 @@ namespace Game.Presentation.Weapons
         [SerializeField] private float _showDuration = 0.08f;
 
         private SignalBus _signalBus;
+        private CancellationTokenSource _showLaserCancellation;
 
         [Inject]
         public void Construct(SignalBus signalBus)
@@ -25,27 +27,44 @@ namespace Game.Presentation.Weapons
 
         private void OnEnable()
         {
+            _showLaserCancellation = new CancellationTokenSource();
             _signalBus.Subscribe<LaserFiredSignal>(OnLaserFired);
         }
 
         private void OnDisable()
         {
-            if (_signalBus == null) return;
-            _signalBus.Unsubscribe<LaserFiredSignal>(OnLaserFired);
+            if (_signalBus != null)
+                _signalBus.Unsubscribe<LaserFiredSignal>(OnLaserFired);
+
+            if (_showLaserCancellation != null)
+            {
+                _showLaserCancellation.Cancel();
+                _showLaserCancellation.Dispose();
+                _showLaserCancellation = null;
+            }
+
+            _lineRenderer.enabled = false;
         }
 
         private void OnLaserFired(LaserFiredSignal signal)
         {
-            ShowLaser(signal.Start, signal.End).Forget();
+            ShowLaserAsync(signal.Start, signal.End, _showLaserCancellation.Token).Forget();
         }
 
-        private async UniTaskVoid ShowLaser(Vector2 start, Vector2 end)
+        private async UniTask ShowLaserAsync(Vector2 start, Vector2 end, CancellationToken cancellationToken)
         {
             _lineRenderer.enabled = true;
             _lineRenderer.SetPosition(0, new Vector3(start.x, start.y, 0f));
             _lineRenderer.SetPosition(1, new Vector3(end.x, end.y, 0f));
 
-            await UniTask.Delay((int)(_showDuration * 1000));
+            try
+            {
+                await UniTask.Delay(System.TimeSpan.FromSeconds(_showDuration), cancellationToken: cancellationToken);
+            }
+            catch (System.OperationCanceledException)
+            {
+                return;
+            }
 
             _lineRenderer.enabled = false;
         }
